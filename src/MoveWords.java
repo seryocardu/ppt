1c3b95e3977585ec349bf3fd1e0fd8197ac356a9
pelop <pelop@RayDark>
2020-02-09 11:56:06 +0100

    Sustituimos LAGARTIJA por TORITO y SPOCK por SEPIA

--- src/MoveWords.java
+++ src/MoveWords.java
@@ -6,7 +6,7 @@ public class MoveWords{
     public static final int GANA = 1;
     public static final int PIERDE = 2;
 
-    private static final String[] validMoves = {"TIJERAS", "LAGARTIJA", "SPOCK", "PAPEL", "PIEDRA"};
+    private static final String[] validMoves = {"TIJERAS", "TORITO", "SEPIA", "PAPEL", "PIEDRA"};
     private static final String[] validCommands = {"SALIR", "HELP"};
 
     private Random rnd;
